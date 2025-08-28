3380b05bda71b7fa0527b31722ed09e17079b464
juanpfr <juanpedrofrediani04@gmail.com>
2025-08-28 20:03:19 -0300

    tudo funcionando

--- galeria/urls.py
+++ galeria/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<int:pk>/', views.photo_detail, name='photo_detail'),
+    path('pesquisar/', views.photo_search, name='photo_search'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Photo
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1>Bem-vindo à Galeria de Fotos!</h1>")
+    photos = Photo.objects.all()
+    return render(request, 'galeria/home.html', {'photos': photos})
+
+def photo_detail(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    return render(request, 'galeria/photo_detail.html', {'photo': photo})
+
+def photo_search(request):
+    query = request.GET.get('q')
+    resultados = []
+    if query:
+        resultados = Photo.objects.filter(title__icontains=query)
+
+    context = {
+        'query': query,
+        'resultados': resultados,
+    }
+    
+    return render(request, 'galeria/photo_search.html', context)
